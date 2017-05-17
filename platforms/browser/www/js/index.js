d9ef74a015d54a246dc2361d2ba8d1691fa66090
Lcng <294867413@qq.com>
2017-05-17 16:25:43 +0800

    1

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -27,6 +27,9 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('load', function () {
+            window.location.href = 'http://www.baidu.com';
+        }, false);
     },
     // deviceready Event Handler
     //
